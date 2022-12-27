e1b7537c32bf16aca5d2d34f2c939ec5ab9d13a8
Michael Dopheide <dopheide@es.net>
2022-12-27 17:01:08 -0600

    Day 17 input

--- 16/aoc16.py
+++ 16/aoc16.py
@@ -30,36 +30,6 @@ def Calculate_Paths(valves):
             tmp_paths = copy.deepcopy(valves[name].paths)
             for path in tmp_paths:
                 # Calculate_Deeper_Paths...
-                for path2 in valves[path].paths:
-                    #                    print(path2)
-                    if path2 not in valves[name].paths:
-                        valves[name].paths[path2] = valves[path].paths[path2] + 2
-                    elif (
-                        valves[path].paths[path2] + valves[name].paths[path]
-                        < valves[name].paths[path2]
-                    ):
-                        valves[name].paths[path2] = (
-                            valves[path].paths[path2] + valves[name].paths[path]
-                        )
-        depth += 1
-
-
-def Calculate_Paths_New(valves):
-    """Pre-Calculate the shortest path between each valve to any other valve
-    Since we're only doing this once it doesn't need to be very efficient"""
-    for name in valves:
-        ## go down each tunnel, and count the distance, populate paths.
-        valves[name].paths[name] = 0
-        for tunnel in valves[name].tunnels:
-            if tunnel not in valves[name].paths:
-                valves[name].paths[tunnel] = 1
-
-    depth = 1
-    while depth <= len(valves):
-        for name in valves:
-            tmp_paths = copy.deepcopy(valves[name].paths)
-            for path in tmp_paths:
-                # Calculate_Deeper_Paths...
 
                 # current distance to this next one is valves[name].paths[path]
 
@@ -97,69 +67,6 @@ def Potentials(location, useful, valves, potentials, min_rem):
         else:
             # already open, it's additive potential is zero
             potentials[valve] = 0
-
-
-def Best_Rate(depth, rates, location, useful, valves, mrem):
-
-    #    rates = copy.deepcopy(ratez)
-    if depth not in rates:
-        rates[depth] = dict()
-
-    for i in range(len(useful)):
-
-        min_rem = mrem
-        cur_useful = copy.deepcopy(useful)
-        #        print(cur_useful)
-        valve = cur_useful[i]
-
-        ## WHY IS JJ ALWAYS LAST?
-
-        dict_useful = {}
-        for c in cur_useful:
-            dict_useful[c] = 0
-
-        del dict_useful[valve]
-
-        ### TODO... still seeing duplicates
-
-        min_rem = min_rem - valves[location].paths[valve]
-
-        if min_rem < 1:
-            # Ya, done!
-            # Rates don't change
-
-            #            print("End", rates)
-            return
-        else:
-            # open
-            min_rem -= 1  # takes 1 min to open valve
-            pressure = min_rem * valves[valve].rate
-
-            # breakpoint()
-
-            # TODO
-            if depth > 0:
-                #### FUUUUUCKK....  this is adding to everything, not just the path that got us here.
-
-                for r in rates[depth - 1]:  # rates[depth] is a dict
-                    rate = rates[depth - 1][r]
-                    new_rate = rate + pressure
-                    new_path = r + valve  # str
-                    rates[depth][new_path] = new_rate
-            else:
-                rates[depth][valve] = pressure
-
-            if len(dict_useful) == 0 or min_rem < 2:
-                # Nowhere to go or we don't have time to get anywhere and do something.
-                #                print("End2", rates)
-                return
-            else:
-                Best_Rate(
-                    depth + 1, rates, valve, list(dict_useful.keys()), valves, min_rem
-                )
-    #                print(rates)
-
-    return
 
 
 def Dammit(depth, rates, location, useful, valves, mrem, p2h):
@@ -232,6 +139,109 @@ def Dammit(depth, rates, location, useful, valves, mrem, p2h):
             )
 
 
+def Best_Rate(depth, rates, location, elocation, useful, valves, mrem, erem, p2h, e2h):
+    """Rnd2:  Need to track a different number of minutes remaining
+    and need the rates data structure to track both paths (m and e)
+    We try to do m and e as part of the dict key (which is the path) 
+    
+    This doesn't work.
+    
+    """
+
+    #    rates = copy.deepcopy(ratez)
+    if depth not in rates:
+        rates[depth] = dict()
+
+    for i in range(len(useful)):
+        min_rem = mrem
+        cur_useful = copy.deepcopy(useful)
+        valve = cur_useful[i]
+
+        dict_useful = {}
+        for c in cur_useful:
+            dict_useful[c] = 0
+        del dict_useful[valve]
+
+        # we lose minutes traveling to the valve
+        min_rem = min_rem - valves[location].paths[valve]
+
+        if min_rem < 1 and erem < 1:
+            # Ya, done!
+            continue  # TODO, don't continue until both m and e are done
+        else:
+            # open
+            min_rem -= 1  # takes 1 min to open valve
+            pressure = min_rem * valves[valve].rate
+
+            if depth == 0:
+                new_path = "m" + valve
+                rates[depth][new_path] = pressure
+                
+                ### rates path name will include who's doing it m or e. (me or elephant)
+
+            else:
+
+                rate = rates[depth - 1][p2h]
+
+                # TODO, need to track mNAME and eNAME
+
+                new_rate = rate + pressure
+                new_path = p2h + valve  # str
+                rates[depth][new_path] = new_rate
+
+        if min_rem < 2:
+            # not enough time to do anything
+            print("skip continue2")
+            #continue  ## TODO, can't continue until both m and e are done
+        
+        ##### TODO.  So I think here we loop over dict_useful and pull one for 'e'
+        # then continue on with min_rem and el_rem for both...
+        el_useful = list(dict_useful.keys())
+        for j in range(len(dict_useful)):
+            edict_useful = copy.deepcopy(dict_useful)
+            el_rem = erem
+            evalve = el_useful[j]
+            del edict_useful[evalve]
+        
+            el_rem = el_rem - valves[elocation].paths[evalve]
+            
+            if el_rem < 1:
+                # Ya, done!
+                print("skip e continue")
+            else:
+                el_rem -= 1  # takes 1 min to open valve
+                epressure = el_rem * valves[evalve].rate + pressure
+
+                if depth == 0:
+                    enew_path = "e" + evalve
+                    rates[depth][enew_path] = epressure
+                else:
+                    rate = rates[depth - 1][e2h]
+                    new_rate = rate + epressure
+                    enew_path = e2h + evalve  # str
+                    rates[depth][enew_path] = new_rate
+            
+            if el_rem < 2:
+                print("skip e continue2")
+
+            if len(edict_useful) > 0:
+        ### TODO, potential issue.  If there's only one left, it might be 
+        # better for the elephant to get it vs me.
+
+                Best_Rate(
+                depth + 1,
+                rates,
+                valve,
+                evalve,
+                list(edict_useful.keys()),
+                valves,
+                min_rem,
+                el_rem,
+                new_path,
+                enew_path
+                )
+
+
 testcase = False
 if testcase:
     file = "test.txt"
@@ -268,7 +278,7 @@ with open(file, "r") as stuff:
     #
     # Let's start here, but I really think there's a problem of the 'valuable' middle valve on a path.
 
-    Calculate_Paths_New(all_valves)
+    Calculate_Paths(all_valves)
 
     # Remove useless valves from further consideration.
     # Okay, we have the paths, but most of these valves are useless to open with rate=0
@@ -291,10 +301,80 @@ with open(file, "r") as stuff:
     rates = {}
 
     path_to_here = ""
-    Dammit(0, rates, location, useful_valves, all_valves, 30, path_to_here)
-    # Best_Rate(0, rates, location, useful_valves, all_valves, 30)
 
-    # pprint.pprint(rates)
+    Rnd1 = False
+    if Rnd1:
+        Dammit(0, rates, location, useful_valves, all_valves, 30, path_to_here)
+        # Best_Rate(0, rates, location, useful_valves, all_valves, 30)
+
+        # pprint.pprint(rates)
+        best = 0
+        bestpath = ""
+        for i in range(len(rates)):
+            if len(rates[i]) > 0:
+                m = max(rates[i], key=rates[i].get)
+                mr = rates[i][m]
+                if mr > best:
+                    best = mr
+                    bestpath = m
+
+        print("Rnd1:", best, bestpath)
+
+
+    Dammit(0, rates, location, useful_valves, all_valves, 26, path_to_here)
+#    pprint.pprint(rates)
+
+
+    ''' okay, we know Dammit() works
+        Let's now just compare the paths that don't contain the same
+        letters
+        Also, we can probably skip depths 0-2'''
+
+    i = 2
+    max = 0
+    print("This takes awhile, use pypy3...")
+    # real	18m26.561s
+    # user	18m17.701s
+    # sys	0m8.101s
+    # we know from previous runs we can start at at least i=4, max 2542
+    while(i < len(rates)):
+        ### Big O no no!
+        j = i
+        print("Comparing Depths:",i,j)
+        for r in rates[i]:
+            me = []
+            mr = r
+            while r:
+                me.append(r[:2])
+                r = r[2:]
+
+
+            j = i  # already compared lower depths with all higher depths
+            while(j < len(rates)):
+                for q in rates[i]:
+                    you = []
+                    mq = q
+                    while q:
+                        you.append(q[:2])
+                        q = q[2:]
+                
+#                print(me,you)
+                    both = list(set().union(me,you))
+                    if(len(both) == (len(me) + len(you))):
+                        total = rates[i][mr] + rates[i][mq]
+                        if(total > max):
+                            max = total
+                            print("New max:",max)
+                j += 1
+        i += 1
+    print("Rnd2 Max:", max)
+
+  #          union of lists == len of list1 plus list2. after splitting into two char pairs
+
+    exit()
+
+    Best_Rate(0, rates, location, location, useful_valves, all_valves, 26, 26, path_to_here, path_to_here)
+    pprint.pprint(rates)
     best = 0
     bestpath = ""
     for i in range(len(rates)):
@@ -305,5 +385,12 @@ with open(file, "r") as stuff:
                 best = mr
                 bestpath = m
 
-    print("Rnd1:", best, bestpath)
+    # need to add e to m somehwere....
+    # try just adding pressure into epressure.
+
+    print("Rnd2:", best, bestpath)
+
+
+
+
 # maybe pypy3 again?
--- 17/aoc17.py
+++ 17/aoc17.py
@@ -2,182 +2,59 @@
 
 import re
 
+class Rock:
+    def __init__(self, shape, y, x):
+        self.width = len(shape[0])
+        self.height = len(shape)
+        self.x = x
+        self.y = y
+        self.shape = shape
 
-class Sensor:
-    def __init__(self, x, y, bx, by):
-        self.bx = bx
-        self.by = by
-        self.range = abs(bx - x) + abs(by - y)  # calculate manhattan distance.
-
-
-testcase = False
-Rnd1 = False
+testcase = True
 if testcase:
     file = "test.txt"
-    testy = 10
+    c_width = 7
+    num_rocks = 2022
 else:
     file = "input.txt"
-    testy = 2000000
+    c_width = 7
+    num_rocks = 2022
+
+# Initialize Rocks
+shapes = []
+shapes.append(Rock(['####']))
+shapes.append(Rock(['.#.',
+                   '###',
+                   '.#.']))
+shapes.append(Rock(['..#',
+                   '..#',
+                   '###']))
+shapes.append(Rock(['#',
+                   '#',
+                   '#',
+                   '#']))
+shapes.append(Rock(['##',
+                   '##']))
 
 with open(file, "r") as stuff:
     lines = stuff.read().splitlines()
 
-    # establish classes for Beacons and Sensors.
-    # Sensors should store the distance to their Beacon, this becomes their current sensor range.
-    # for each position in the row in question, Calculate it's distance to each sensor, if it's further
-    # than all of them, it could be a beacon.  We want the inverse of that, positions that can't be beacons and are
-    # IN range of a sensor, but not the sensor's beacon itself (skip known beacon locations)
-
-    # Assuming we need to start counting our row somewhere in negative X space, so we also need to determine
-    # what our most negative X range actually IS based on the further range of the sensors.
-
-    sensors = {}
-    beacons = {}
-
+    stack = []  # stack of rocks
+    stack_height = 0
+    active = False  # is there an active rock?
+    next_shape = 0
     for line in lines:
-        m = re.search(r"x=(-?\d+), y=(-?\d+).*x=(-?\d+), y=(-?\d+)", line)
-        if m:
-            sensorx = int(m.group(1))
-            sensory = int(m.group(2))
-            beaconx = int(m.group(3))
-            beacony = int(m.group(4))
+        print(line)
+        print("BLAA")
+    exit()
 
-            sensors[(sensorx, sensory)] = Sensor(sensorx, sensory, beaconx, beacony)
-            beacons[(beaconx, beacony)] = 1
-        else:
-            print("error, regex failed", line)
-            exit()
+    while(len(stack) <= num_rocks):
+        if(not active):
+            # Generate new rock
+            The_Rock = Rock(shapes[next_shape],stack_height+4, 3)
+            next_shape += 1
+            if(next_shape == len(shapes)):
+                next_shape = 0
 
-    # Figure out the size of our field.
-    first = 1
-    for sensor in sensors:
-        (x, y) = sensor
-        srange = sensors[(x, y)].range
-        if first:
-            minx = x - srange
-            maxx = x + srange
-            miny = y - srange
-            maxy = y + srange
-            first = 0
-            continue
-        if x - srange < minx:
-            minx = x - srange
-        if x + srange > maxx:
-            maxx = x + srange
-        if y - srange < miny:
-            miny = y - srange
-        if y + srange > maxy:
-            maxy = y + srange
 
-    if Rnd1:
-        print("X", minx, maxx)
-        print("Y", miny, maxy)
-        i = minx
-        not_a_beacon = 0
-        while i <= maxx:
-            # current = (i,testy)
 
-            # for each position, check to see if we're within range of each sensor.
-            # this will not scale.
-            for sensor in sensors:
-                (x, y) = sensor
-                distance = abs(x - i) + abs(y - testy)
-                if distance <= sensors[sensor].range:
-                    # cannot be a beacon (or already is)
-                    if (i, testy) not in beacons:
-                        not_a_beacon += 1
-                        #                    print("Not a beacon")
-                        break
-            i += 1
-
-        print("Rnd1 (not a beacon):", not_a_beacon)
-
-    ## Rnd2 ##
-
-    minx = 0
-    miny = 0
-    if testcase:
-        maxx = 20
-        maxy = 20
-    else:
-        maxx = 4000000
-        maxy = 4000000
-
-    print("X", minx, maxx)
-    print("Y", miny, maxy)
-
-    # so I move until I find a sensor.  calculate it's distance, but also veritcal distance from me.
-    # (figure out where in the diamond I am).  Then skip the rest of the diamond.
-    # Keep in mind I might not enter a sensor range right at the edge.
-
-    y = 0
-    while y < maxy:
-
-        x = 0
-        while x < maxx:
-
-            # find closest sensor
-
-            # TODO, maybe my dist vs smallest is wrong?
-
-            shortest = None
-            for sensor in sensors:
-                (sx, sy) = sensor
-                dist = abs(x - sx) + abs(y - sy)
-                if (
-                    dist <= sensors[sensor].range
-                ):  # only do diffs if we're inside the range.
-                    if shortest == None:
-                        shortest = dist
-                        current_sensor = (sx, sy)
-                    elif dist < shortest:
-                        shortest = dist
-                        current_sensor = (sx, sy)
-
-            if shortest == None:
-                print("Holy Crap!")
-                print("Rnd2:", (x, y))
-                print("Rnd2 Freq: ", x * 4000000 + y)
-                exit()
-            #            print("X, Y", x, y)
-            #            print("Dist:", shortest)
-            #            print("Current Sensor:", current_sensor)
-            #            print("Sensor Range: ", sensors[current_sensor].range)
-            if x < 0:
-                print("Error x<0, Exit")
-                exit()
-            #
-            (cx, cy) = current_sensor
-            xdiff = abs(cx - x)
-            ydiff = abs(cy - y)
-            #            print("Xdiff:", xdiff)
-            #            print("Ydiff:", ydiff)
-
-            ## At the closest sensor we can skip to it's furtherest edge at this 'height'
-
-            if x > cx:
-                # Move X + range at the level of y we're at.
-                #                x += sensors[current_sensor].range - ydiff
-                change = sensors[current_sensor].range - xdiff - ydiff
-                if change == 0:
-                    x += 1
-                elif change < 0:
-                    print("error 3")
-                    exit()
-                else:
-                    x += change
-
-                #                print("New X:", x)
-                continue
-            elif x < cx:
-                x += xdiff + sensors[current_sensor].range - ydiff
-                #                x += 2 * sensors[current_sensor].range - ydiff * 2
-                #                print("New 2X:", x)
-                continue
-
-            x += 1
-
-            # this is the closest sensor to us, find out where in it's range we are.
-            # then skip to it's edge.
-        y += 1
-    # use pypy3
