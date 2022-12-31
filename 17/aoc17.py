1e73437f94dfd20e88e903549e4bf1e2642972fe
Michael Dopheide <dopheide@es.net>
2022-12-30 21:06:25 -0600

    Day 17 Done.

--- 17/aoc17.py
+++ 17/aoc17.py
@@ -19,7 +19,7 @@ def Check_Path_Left(rock, fuck_shit_stack, c_width):  # Reggie Watts reference
     if(rock.x == 1):
         # up against the left edge already
         result = False
-    elif y <= len(stack):
+    elif y <= len(fuck_shit_stack):
         # We're lower than the top of the stack
         rock_edge = []
         stack_x = rock.x - 2  # -1 for index, -1 for position
@@ -30,14 +30,14 @@ def Check_Path_Left(rock, fuck_shit_stack, c_width):  # Reggie Watts reference
         row = len(rock_edge) - 1
         while(y <= len(fuck_shit_stack)):
 #            print("Comp", rock_edge[row], stack[y-1][stack_x])
-            if(rock_edge[row]) == '#' and stack[y-1][stack_x] == '#':
+            if(rock_edge[row]) == '#' and fuck_shit_stack[y-1][stack_x] == '#':
                 result = False
             elif(rock_edge[row] == '.'):
                 # check further
                 j = 0
                 while(rock.shape[row][j] == '.' and j < len(rock.shape[0])):
                     next_edge = rock.shape[row][j+1]
-                    if(next_edge == "#" and stack[y-1][stack_x+j+1] == "#"):
+                    if(next_edge == "#" and fuck_shit_stack[y-1][stack_x+j+1] == "#"):
                         result = False
                     j += 1
 
@@ -56,7 +56,7 @@ def Check_Path_Right(rock, fuck_shit_stack, c_width):  # Reggie Watts reference
     if(rock.x + rock.width - 1) == c_width:
         # up against the right edge already
         result = False
-    elif y <= len(stack):
+    elif y <= len(fuck_shit_stack):
         # We're lower than the top of the stack
         rock_edge = []
         stack_x = rock.x + rock.width - 1
@@ -65,7 +65,7 @@ def Check_Path_Right(rock, fuck_shit_stack, c_width):  # Reggie Watts reference
         
         row = len(rock_edge) - 1
         while(y <= len(fuck_shit_stack)):
-            if(rock_edge[row]) == '#' and stack[y-1][stack_x] == '#':
+            if(rock_edge[row]) == '#' and fuck_shit_stack[y-1][stack_x] == '#':
                 result = False
 
             elif(rock_edge[row] == '.'):
@@ -74,7 +74,7 @@ def Check_Path_Right(rock, fuck_shit_stack, c_width):  # Reggie Watts reference
                 j = 0
                 while(rock.shape[row][j] == '.' and j < len(rock.shape[0])):
                     next_edge = rock.shape[row][rock.width - j - 2]
-                    if(next_edge == "#" and stack[y-1][stack_x-j-1] == "#"):
+                    if(next_edge == "#" and fuck_shit_stack[y-1][stack_x-j-1] == "#"):
                         result = False
                     j += 1
 
@@ -161,7 +161,27 @@ def Print_Stack(stack):
         print("|%s|" % row)
     print("+-------+")
 
-testcase = False
+def Print_TopBot_Stack(stack):
+    ''' Print stack top-down (in reverse) '''
+
+    for i in reversed(range(len(stack))):
+        if(i >= 0 and i<=5) or (i <= len(stack) and i >= len(stack)-5):
+            row = stack[i]
+            row = ('').join(row)
+            print("|%s|" % row)
+    print("+-------+")
+
+def Print_Top_Stack(stack):
+    ''' Print stack top-down (in reverse) '''
+
+    for i in reversed(range(len(stack))):
+        if(i <= len(stack) and i >= len(stack)-20):
+            row = stack[i]
+            row = ('').join(row)
+            print("|%s|" % row)
+    print("+-------+")
+
+testcase = True
 if testcase:
     file = "test.txt"
     c_width = 7
@@ -188,68 +208,164 @@ shapes.append(['##',
                '##'])
 
 
+def Calculate_Stack(multiple,cur_stack,remainder):
+    with open(file, "r") as stuff:
+        lines = stuff.read().splitlines()
 
-with open(file, "r") as stuff:
-    lines = stuff.read().splitlines()
+        stack = []  # stack of rocks (list of lists)
+        active = False  # is there an active rock?
+        next_shape = 0
+        air = list(lines[0])
+        next_air = 0
+        cur_rock = 0
 
-    stack = []  # stack of rocks (list of lists)
-    active = False  # is there an active rock?
-    next_shape = 0
-    air = list(lines[0])
-    next_air = 0
-    cur_rock = 0
+        # Rnd2 calculate repeating num of rocks
+        if(remainder == -1):
+            num_rocks = len(air) * len(shapes) * multiple
+        else:
+            num_rocks = remainder
+            stack = cur_stack
 
-    while(cur_rock <= num_rocks):
-        if(not active):
-            # Generate new rock
-            cur_rock += 1
-            The_Rock = Rock(shapes[next_shape], 3, len(stack)+4, )
-#            if(cur_rock == num_rocks):
-#                Print_Stack(stack)
-#                print(shapes[next_shape])
-            next_shape += 1
-            if(next_shape == len(shapes)):
-                next_shape = 0
-            active = True
+
+        while(cur_rock <= num_rocks):
+            if(not active):
+                # Generate new rock
+                cur_rock += 1
+                The_Rock = Rock(shapes[next_shape], 3, len(stack)+4, )
+                next_shape += 1
+                if(next_shape == len(shapes)):
+                    next_shape = 0
+                active = True
         
-        #######
-        # AIR #
-        #######
+            #######
+            # AIR #
+            #######
 
-#        if(cur_rock == num_rocks):
-#            print("Air:",air[next_air])
-
-        if(air[next_air] == "<"):
-            # try left.
-            if(Check_Path_Left(The_Rock, stack, c_width)):
-                The_Rock.x -= 1
-        else:
-            # try right
-            if(Check_Path_Right(The_Rock, stack, c_width)):
-                The_Rock.x += 1
-
-        next_air += 1
-        if(next_air == len(air)):
-            next_air = 0
-
-        ##################
-        # The Rock falls #
-        ##################
-
-        if(The_Rock.y > len(stack) + 1):
-            # just fall
-            The_Rock.y -= 1
-        elif(The_Rock.y == 1 and len(stack) == 0):   # move logic lower, this only happens once or twice
-            # stop falling
-            Add_to_Stack(The_Rock, stack, c_width)
-            active = False
-#            continue
-        else:
-            if(Check_Path_Down(The_Rock, stack, c_width)):
-                The_Rock.y -= 1
+            if(air[next_air] == "<"):
+                # try left.
+                if(Check_Path_Left(The_Rock, stack, c_width)):
+                    The_Rock.x -= 1
             else:
+                # try right
+                if(Check_Path_Right(The_Rock, stack, c_width)):
+                    The_Rock.x += 1
+
+            next_air += 1
+            if(next_air == len(air)):
+                next_air = 0
+
+            ##################
+            # The Rock falls #
+            ##################
+
+            if(The_Rock.y > len(stack) + 1):
+                # just fall
+                The_Rock.y -= 1
+            elif(The_Rock.y == 1 and len(stack) == 0):   # move logic lower, this only happens once or twice
+                # stop falling
                 Add_to_Stack(The_Rock, stack, c_width)
                 active = False
+            else:
+                if(Check_Path_Down(The_Rock, stack, c_width)):
+                    The_Rock.y -= 1
+                else:
+                    Add_to_Stack(The_Rock, stack, c_width)
+                    active = False
+#    print("Rnd1 Stack Height:",len(stack))
+
+    return stack, num_rocks
 
 #    Print_Stack(stack)
-    print("Rnd1 Stack Height:",len(stack))
+#    print("Rnd1 Stack Height:",len(stack))
+
+
+    # Okay, we can't keep track of the whole stack, but...
+    # We can be confident rocks won't proceed if there is a solid left to right blockage
+    # which doesn't have to be all on one level.
+    # convert stack to dict so it's indexes are still right?
+    # len(stack) will no longer work.
+
+    # Nope, screw that. 
+    # The input loops and the rocks loop.
+    # Calculate a stack that's len(input) * number of shapes.
+    # That's approx 50000, then just sit it on top of a copy of itself.
+    # (no wind shifting when that happens)
+    # figure out the total after that and just multiply.
+
+#    Print_TopBot_Stack(stack)
+
+    ## Bah, not that simple, some shapes could slip through the gaps and change the
+    ## next height
+
+    ## Maybe we can search for a multiple that repeats exactly and then add the new remainder
+
+    # Okay, we're going to track the 'top' of the stack for each mulitple of fully repeating
+    # input.  If we find a place where the top's match, that series will repeat forever.
+    # It looks like this happens in the test input at 1 + (7 * x)
+
+#    Print_Top_Stack(stack)
+
+test = False
+m = 1
+stacks = {}
+while(not test):
+    if(m % 10 == 0):
+        print("Multiple",m)
+
+    (m_stack, num_rocks) = Calculate_Stack(m,[],-1)
+
+    # Assumption: comparing the top 20 rows of the stack will be sufficient.
+    t_stack = m_stack[-20:]
+#    Print_Stack(m_stack)
+
+    if(len(stacks) > 0):
+        for s in stacks:
+            (st,l) = stacks[s]
+            if(st == t_stack):
+#                print("Match",s,m)
+                test = True
+                break
+    stacks[m] = (t_stack,len(m_stack))
+
+    if(not test):
+        m += 1
+
+total_size = 0
+(st,start_size) = stacks[s]
+total_size += start_size
+(st,match_size) = stacks[m]
+diff_size = match_size - start_size
+rocks_per_loop = num_rocks / m
+
+big_multi = int (( 1000000000000 - (rocks_per_loop * s)) / (rocks_per_loop * (m-s)))
+remainder = ( 1000000000000 - (rocks_per_loop * s)) % (rocks_per_loop * (m-s))
+
+total_size += diff_size * big_multi
+
+print("Rnd2 Rocks / Loop:",rocks_per_loop)
+print("Start multiple:",s)
+print("Repeat multiple:",m)
+print("Diff Size", diff_size)
+print("Big Multiple", big_multi)
+print("Remaining rocks", remainder)
+print("Almost Total Size:",total_size)
+
+# okay, we still need to add the remainder to the stack.
+
+(s_stack, x) = Calculate_Stack(s,[],-1)
+
+s_len = len(s_stack)
+print("Length s_stack", s_len)
+
+(a_stack, num_rocks) = Calculate_Stack(0,[],s*rocks_per_loop + remainder)
+
+print("Len a_stack",len(a_stack))
+final_adds = len(a_stack) - s_len
+print("Final", final_adds)
+
+print("Total Size:",total_size+final_adds)
+
+
+
+
+
