7c0275e7e3f54264e20a1653af058e0e30b0ab2b
Michael Dopheide <dopheide@es.net>
2023-01-10 14:31:13 -0600

    Almost there with Day 24 Part 1

--- 24/aoc24.py
+++ 24/aoc24.py
@@ -18,10 +18,13 @@ def BuildMap(lines):
             map[y].append( list(chars[x] ))
     return map
 
-def PrintMap(map):
+def PrintMap(e,map):
+    (ey,ex) = e
     for y in range(len(map)):
         for x in range(len(map[y])):
-            if(len(map[y][x]) == 1):
+            if(y == ey and x == ex):
+                print('E',end='')
+            elif(len(map[y][x]) == 1):
                 print(map[y][x][0],end='')
             else:
                 print(len(map[y][x]),end='')
@@ -31,9 +34,6 @@ def Blizzards(map):
     '''
     Determine all of the blizzards' new positions.  A little tricky because we only want to move
     each blizzard once.  Maybe try writing them to a new map.
-
-    TODO: Potential improvement if cpu time becomes an issue.  In exchange for memory we can store the
-    map at any given minute so we don't have to recursively recalculate the same minute over and over.
     '''
 
     # build new empty map (there's gotta be a better way)
@@ -47,8 +47,11 @@ def Blizzards(map):
     for y in range(len(map)):
         for x in range(len(map[y])):
             if(map[y][x][0] != '.' and map[y][x][0] != '#'):
-                while(len(map[y][x]) > 0):
-                    bliz = map[y][x].pop()
+
+#                while(len(map[y][x]) > 0):
+#                    bliz = map[y][x].pop()
+                for i in range(len(map[y][x])):
+                    bliz = map[y][x][i]
                     if(bliz == '<'):
                         # check for edge
                         if(x - 1 == 0):
@@ -80,28 +83,21 @@ def Blizzards(map):
             if(len(new_map[y][x]) == 0):
                 new_map[y][x].append('.')
 
-    return new_map 
+    return new_map
 
 def Search_Moves(e, map):
     (y, x) = e
 
-    # verify extents
-
     options = []
 
-    print("HERE")
-    # need to check extents, but also we're missing some '.'s in the map.
-    # where'd they go.
-
-    if(y < len(map)-1):
-        if(map[y+1][x][0] == '.'):
-            options.append('down')
-
+    if(map[y+1][x][0] == '.'):
+        options.append('down')
     if(map[y][x+1][0] == '.'):
         options.append('right')
     if(map[y][x-1][0] == '.'):
         options.append('left')
-    if(map[y-1][x][0] == '.'):
+
+    if(map[y-1][x][0] == '.' and y>1):  # don't move up from home
         options.append('up')
     #do 'stay' last, it's the least likely to help in the long run.
     if(map[y][x][0] == '.'):
@@ -114,6 +110,9 @@ def Move(e,map,clock):
     '''
     
     TODO: optimization, if clock > any successful return count, end that attempt
+
+    Assumption, it appears we're allowed to move 'through' a blizzard as long as
+    we don't end up in the same space.
     '''
     # options are stay, up, down, left right
     # keep in mind a blizzard may move to where we are now.
@@ -121,65 +120,74 @@ def Move(e,map,clock):
     options = Search_Moves(e, map)
 
     if(len(options) == 0):
-        "Dead End"
+#        print("Dead End")
         return -1
 
-    if(clock > 20):
-        print("Max clock reached")
+    if(clock > 100):
+#        print("Max clock reached")
         return -1
 
     new_map = Blizzards(map) # this is the new blizzard positions at clock + 1
     clock += 1
 
+    ## We're going to need to keep track of where we've been and not go back there too many times.
+
     (y, x) = e
     min_ret = 0
     for op in options:
-        print("do:",op)
+#        print("clock:",clock,op,e)
         if(op == 'down'):
             e = (y+1,x)
             if(e == (len(map)-1,len(map[0])-2)):
                 print("Found the End!",clock-1)
-                ret = clock - 1
+                return clock - 1
+#                ret = clock - 1
             else:
                 ret = Move(e,new_map,clock)
+                if(ret != -1 and ret != 0):
+                    if(min_ret == 0):
+                        min_ret = ret
+                    elif(ret < min_ret):
+                        min_ret = ret
+        elif(op == 'left'):
+            e = (y,x-1)
+            ret = Move(e,new_map,clock)
+            if(ret != -1 and ret != 0):
                 if(min_ret == 0):
                     min_ret = ret
-                elif(ret < min_ret and ret != -1):
+                elif(ret < min_ret):
                     min_ret = ret
-        if(op == 'left'):
-            e = (y,x-1)
+        elif(op == 'right'):
+            e = (y,x+1)
             ret = Move(e,new_map,clock)
-            if(min_ret == 0):
-                min_ret = ret
-            elif(ret < min_ret and ret != -1):
-                min_ret = ret
-        if(op == 'right'):
-            e = (y,x-1)
-            ret = Move(e,new_map,clock)
-            if(min_ret == 0):
-                min_ret = ret
-            elif(ret < min_ret and ret != -1):
-                min_ret = ret            
-        if(op == 'up'):
+            if(ret != -1 and ret != 0):
+                if(min_ret == 0):
+                    min_ret = ret
+                elif(ret < min_ret):
+                    min_ret = ret            
+        elif(op == 'up'):
             e = (y-1,x)
             ret = Move(e,new_map,clock)
-            if(min_ret == 0):
-                min_ret = ret
-            elif(ret < min_ret and ret != -1):
-                min_ret = ret
-        if(op == 'stay'):
+            if(ret != -1 and ret != 0):
+                if(min_ret == 0):
+                    min_ret = ret
+                elif(ret < min_ret):
+                    min_ret = ret
+        elif(op == 'stay'):
             # need to do something to prevent the loop where it stays at home forever.
-            if(clock < 10 and e == (0,1)):
+            if(clock > 10 and e == (0,1)):
                 print("skip stay at home")
             else:
                 ret = Move(e,new_map,clock)
-                if(min_ret == 0):
-                    min_ret = ret
-                elif(ret < min_ret and ret != -1):
-                    min_ret = ret
+                if(ret != -1 and ret != 0):
+                    if(min_ret == 0):
+                        min_ret = ret
+                    elif(ret < min_ret):
+                        min_ret = ret
     return min_ret
 
-testcase = True
+
+testcase = False
 if testcase:
     file = "test.txt"
 else:
@@ -196,8 +204,11 @@ map = BuildMap(lines)
 e = (0,1) # expedition starting location
 
 map = Blizzards(map)
+
 minimum = Move(e, map,1) # map, minute
 print("Rnd1:",minimum)
+
+print("If nothing, increase max clock and probably need to implement some optimizations")
 
 # determine next blizzard positions, we can't stop that from happening.
 # since blizzards can overlap and we still have to know where each is headed
